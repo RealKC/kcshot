53f364376495d9b914089da4187efd353f385033
Mițca Dumitru <dumitru0mitca@gmail.com>
2021-10-19 10:40:35 +0300

    Nicer erorr handling and add screenshot modules in screenshot module
    We add an Error enum to name possible errors and make functions use it.
    
    Furthermore every usage of unsafe is now matched by a comment starting
    with `SAFETY:` detailing why the calls are safe (as far as I undertsand
    the functions used).

--- src/editor/screenshot.rs
+++ src/editor/screenshot.rs
@@ -1,10 +1,12 @@
 use std::{
     ffi::CString,
     fs::File,
+    io,
     os::{
         raw::{c_char, c_int, c_uint},
         unix::io::FromRawFd,
     },
+    ptr::NonNull,
 };
 
 use cairo::{
@@ -27,64 +29,126 @@ extern "C" {
     ) -> cairo_status_t;
 }
 
-struct XDisplay(*mut x11::xlib::_XDisplay);
+struct XDisplay(NonNull<x11::xlib::_XDisplay>);
 
 impl XDisplay {
-    fn open_default() -> Self {
-        Self(unsafe { XOpenDisplay(std::ptr::null_mut()) })
+    fn open_default() -> Result<Self, Error> {
+        // SAFETY: This call is valid
+        //         See: https://www.x.org/releases/X11R7.7/doc/libX11/libX11/libX11.html#Opening_the_Display
+        NonNull::new(unsafe { XOpenDisplay(std::ptr::null()) })
+            .ok_or(Error::FailedToOpenDisplay)
+            .map(Self)
+    }
+
+    fn as_ptr(&self) -> *mut x11::xlib::_XDisplay {
+        self.0.as_ptr()
     }
 }
 
 impl Drop for XDisplay {
     fn drop(&mut self) {
-        unsafe { XCloseDisplay(self.0) };
+        // SAFETY: This call is okay as `self.0` was obtained from a call to XOpenDisplay
+        //         Also see https://www.x.org/releases/X11R7.7/doc/libX11/libX11/libX11.html#Closing_the_Display
+        //         As this is called in a drop impl, and due to the way Xlib resources are managed, this is fine.
+        unsafe { XCloseDisplay(self.as_ptr()) };
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Encountered an error from cairo: {0}")]
+    Cairo(#[from] CairoError),
+    #[error("Encountered an I/O error: {0}")]
+    Io(#[from] io::Error),
+    #[error("Failed to obtain an XDisplay. (Is DISPLAY set? Is an X server running?)")]
+    FailedToOpenDisplay,
+    #[error("Failed to fetch the root window where the cursor was from the X server")]
+    FailedToGetRootWindow,
+    #[error("Failed to get window attributes")]
+    FailedToGetWindowAttributes,
+}
+
+impl From<cairo::IoError> for Error {
+    fn from(cerr: cairo::IoError) -> Self {
+        match cerr {
+            cairo::IoError::Cairo(cairo) => Self::Cairo(cairo),
+            cairo::IoError::Io(io) => Self::Io(io),
+        }
     }
 }
 
 #[tracing::instrument]
-pub fn take_screenshot() -> Option<ImageSurface> {
+pub fn take_screenshot() -> Result<ImageSurface, Error> {
     info!("entered");
-    let display = XDisplay::open_default();
+    let display = XDisplay::open_default()?;
     let (window_where_cursor_is, screen_where_the_cursor_is) =
-        find_window_where_cursor_is(display.0)?;
+        find_window_where_cursor_is(display.as_ptr())?;
 
+    // SAFETY: This is a C struct so this should be a valid byte pattern for it. I hope.
     let mut attributes: XWindowAttributes = unsafe { std::mem::zeroed() };
-    // FIXME: Figure out how to handle possible errors
-    unsafe { XGetWindowAttributes(display.0, window_where_cursor_is, &mut attributes as _) };
+    // SAFETY: All the arguments are valid
+    let rc = unsafe {
+        XGetWindowAttributes(
+            display.as_ptr(),
+            window_where_cursor_is,
+            &mut attributes as _,
+        )
+    };
+    if rc == 0 {
+        return Err(Error::FailedToGetWindowAttributes);
+    }
 
-    let visual = unsafe { XDefaultVisual(display.0, screen_where_the_cursor_is) };
+    // SAFETY: All the arguments are valid
+    let visual = unsafe { XDefaultVisual(display.as_ptr(), screen_where_the_cursor_is) };
 
+    // SAFETY: All the arguments are valid, but I think cairo will return an error surface even if one is not?
     let screenshot = unsafe {
         ffi::cairo_xlib_surface_create(
-            display.0,
+            display.as_ptr(),
             window_where_cursor_is,
             visual,
             attributes.width,
             attributes.height,
         )
     };
+    // Just to be sure
+    debug_assert!(!screenshot.is_null());
 
-    let path = CString::new("screenshot.XXXXXX.png").unwrap().into_raw();
-    let fd = unsafe { libc::mkstemps(path, 4) };
-    assert!(fd != -1);
-
-    let rc = unsafe { cairo_surface_write_to_png(screenshot, path as _) };
-    match rc {
+    // SAFETY: `screenshot` is nonnull
+    match unsafe { ffi::cairo_surface_status(screenshot) } {
         0 => {}
-        err => {
-            error!("{:?}", CairoError::from(err));
-            panic!()
-        }
+        err => return Err(CairoError::from(err).into()),
     }
 
-    // To deallocate that memory
-    let _path = unsafe { CString::from_raw(path) };
+    let template = CString::new("screenshot.XXXXXX.png")
+        .expect("CString::new shouldn't fail with string literals AFAIK")
+        .into_raw();
 
+    // SAFETY: `path` is a nonnull, and a valid pointer. Also even if the template is invalid,
+    //         `mkstemps` should just return an error in that case.
+    let fd = unsafe { libc::mkstemps(template, 4) };
+    // SAFETY: We got this pointer earlier from calling CString::into_raw so this should be ok.
+    let path = unsafe { CString::from_raw(template) };
+    if fd == -1 {
+        return Err(io::Error::last_os_error().into());
+    }
+
+    // SAFETY: This is the only call to `from_raw_fd` for this specific fd, furthermore it's verified
+    //         to be a valid fd just above.
     let mut file = unsafe { File::from_raw_fd(fd) };
-    ImageSurface::create_from_png(&mut file).ok()
+
+    // SAFETY: All the arguments are valid
+    match unsafe { cairo_surface_write_to_png(screenshot, path.as_ptr()) } {
+        0 => {}
+        err => return Err(CairoError::from(err).into()),
+    }
+
+    Ok(ImageSurface::create_from_png(&mut file)?)
 }
 
-fn find_window_where_cursor_is(display: *mut Display) -> Option<(Window, c_int)> {
+fn find_window_where_cursor_is(display: *mut Display) -> Result<(Window, c_int), Error> {
+    // SAFETY: `display` isn't null, and it's obtained from a good call to XOpenDisplay, should be
+    //         good?
     let screen_count = unsafe { XScreenCount(display) };
 
     for screen in 0..screen_count {
@@ -96,6 +160,10 @@ fn find_window_where_cursor_is(display: *mut Display) -> Option<(Window, c_int)>
         let mut child = Window::default();
         let mut window_with_cursor = Window::default();
 
+        // See:
+        //  * XQueryPointer: https://www.x.org/releases/X11R7.7/doc/man/man3/XQueryPointer.3.xhtml
+        // SAFETY: All arguments should be guaranteed valid, except for the value returned by XRootWindow
+        //         However, if XRootWindow returns an invalid Window, XQueryPointer will return an error
         let rc = unsafe {
             XQueryPointer(
                 display,
@@ -111,9 +179,9 @@ fn find_window_where_cursor_is(display: *mut Display) -> Option<(Window, c_int)>
         };
 
         if rc == x11::xlib::True {
-            return Some((window_with_cursor, screen));
+            return Ok((window_with_cursor, screen));
         }
     }
 
-    None
+    Err(Error::FailedToGetRootWindow)
 }
