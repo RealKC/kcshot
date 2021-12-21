70c41d66a315bf3215817a78692da9545b3ad01c
Mițca Dumitru <dumitru0mitca@gmail.com>
2021-12-21 23:29:47 +0200

    Rename postcapture::{Save => SaveAndCopy} and add docs

--- src/postcapture.rs
+++ src/postcapture.rs
@@ -9,12 +9,14 @@ pub trait PostCaptureAction {
 
 pub fn current_action() -> &'static dyn PostCaptureAction {
     // FIXME: Eventually this should do more than just this, but we'll get there
-    &Save
+    &SaveAndCopy
 }
 
-struct Save;
+/// This struct represents an action that saves the screenshot to disk and then copies it into
+/// the user's clipboard
+struct SaveAndCopy;
 
-impl PostCaptureAction for Save {
+impl PostCaptureAction for SaveAndCopy {
     fn handle(&self, pixbuf: Pixbuf) {
         let now = chrono::Local::now();
         let res = pixbuf.savev(format!("screenshot_{}.png", now.to_rfc3339()), "png", &[]);
