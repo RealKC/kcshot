ec03f892fab1fecab6d333c6ea35be81008e6089
Mițca Dumitru <dumitru0mitca@gmail.com>
2022-01-10 01:01:13 +0200

    Migrate to `xcb` instead of `x11`
    `xcb` offers a generally more rusty API, which makes working with it a
    lot easier. It will also make implementing window selection not a PITA.
    
    And finally, it enables removing most of the `unsafe` usages in
    display_server.rs.

--- src/editor/display_server.rs
+++ src/editor/display_server.rs
@@ -1,24 +1,20 @@
 use std::{
     ffi::CString,
-    fs::File,
     io,
-    os::{
-        raw::{c_char, c_int, c_uint},
-        unix::io::FromRawFd,
-    },
-    ptr::NonNull,
+    os::{raw::c_char, unix::prelude::OsStrExt},
 };
 
 use cairo::{
     self,
-    ffi::{self, cairo_status_t, cairo_surface_t},
+    ffi::{
+        cairo_status_t, cairo_surface_status, cairo_surface_t, cairo_xcb_surface_create,
+        STATUS_SUCCESS as CAIRO_STATUS_SUCCESS,
+    },
     Error as CairoError, ImageSurface,
 };
-use tracing::{error, info, warn};
-use x11::xlib::{
-    Display, Window, XCloseDisplay, XDefaultVisual, XGetWindowAttributes, XOpenDisplay,
-    XQueryPointer, XRootWindow, XScreenCount, XWindowAttributes,
-};
+use gtk4::prelude::{FileExt, IOStreamExt, InputStreamExtManual};
+use tracing::error;
+use xcb::{x, Xid};
 
 extern "C" {
     /// cairo-rs doesn't expose it in its ffi module, so I have to write its declaration myself
@@ -29,43 +25,22 @@ extern "C" {
     ) -> cairo_status_t;
 }
 
-struct XDisplay(NonNull<x11::xlib::_XDisplay>);
-
-impl XDisplay {
-    fn open_default() -> Result<Self, Error> {
-        // SAFETY: This call is valid
-        //         See: https://www.x.org/releases/X11R7.7/doc/libX11/libX11/libX11.html#Opening_the_Display
-        NonNull::new(unsafe { XOpenDisplay(std::ptr::null()) })
-            .ok_or(Error::FailedToOpenDisplay)
-            .map(Self)
-    }
-
-    fn as_ptr(&self) -> *mut x11::xlib::_XDisplay {
-        self.0.as_ptr()
-    }
-}
-
-impl Drop for XDisplay {
-    fn drop(&mut self) {
-        // SAFETY: This call is okay as `self.0` was obtained from a call to XOpenDisplay
-        //         Also see https://www.x.org/releases/X11R7.7/doc/libX11/libX11/libX11.html#Closing_the_Display
-        //         As this is called in a drop impl, and due to the way Xlib resources are managed, this is fine.
-        unsafe { XCloseDisplay(self.as_ptr()) };
-    }
-}
-
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Encountered an error from cairo: {0}")]
     Cairo(#[from] CairoError),
     #[error("Encountered an I/O error: {0}")]
     Io(#[from] io::Error),
-    #[error("Failed to obtain an XDisplay. (Is DISPLAY set? Is an X server running?)")]
-    FailedToOpenDisplay,
-    #[error("Failed to fetch the root window where the cursor was from the X server")]
-    FailedToGetRootWindow,
-    #[error("Failed to get window attributes")]
-    FailedToGetWindowAttributes,
+    #[error("Failed to estabilish a connection to the X server: {0:?}")]
+    XcbConnection(#[from] xcb::ConnError),
+    #[error("Encountered an X protocol error: {0:?}")]
+    XcbProtocol(xcb::ProtocolError),
+    #[error("Got an error trying to make a temporary file: {0}")]
+    TempFile(#[from] gtk4::glib::Error),
+    #[error("Failed to take screenshot. (No root screens? No cursor?)")]
+    FailedToTakeScreenshot,
+    #[error("Failed to get screen resolution. (No root screens? No root windows on the screens that exist?")]
+    FailedToGetScreenResolution,
 }
 
 impl From<cairo::IoError> for Error {
@@ -77,73 +52,98 @@ impl From<cairo::IoError> for Error {
     }
 }
 
-#[tracing::instrument]
+impl From<xcb::Error> for Error {
+    fn from(xerror: xcb::Error) -> Self {
+        match xerror {
+            xcb::Error::Connection(err) => Self::XcbConnection(err),
+            xcb::Error::Protocol(err) => Self::XcbProtocol(err),
+        }
+    }
+}
+
+// Some things in this file are inspired by the code written here https://giters.com/psychon/x11rb/issues/328
+// archive.org link: https://web.archive.org/web/20220109220701/https://giters.com/psychon/x11rb/issues/328 [1]
+
 pub fn take_screenshot() -> Result<ImageSurface, Error> {
-    info!("entered");
-    let display = XDisplay::open_default()?;
-    let (window_where_cursor_is, screen_where_the_cursor_is) =
-        find_window_where_cursor_is(display.as_ptr())?;
+    let (connection, _) = xcb::Connection::connect(None)?;
+    let setup = connection.get_setup();
 
-    // SAFETY: This is a C struct so this should be a valid byte pattern for it. I hope.
-    let mut attributes: XWindowAttributes = unsafe { std::mem::zeroed() };
-    // SAFETY: All the arguments are valid
-    let rc = unsafe {
-        XGetWindowAttributes(
-            display.as_ptr(),
-            window_where_cursor_is,
-            &mut attributes as _,
-        )
-    };
-    if rc == 0 {
-        return Err(Error::FailedToGetWindowAttributes);
+    for root_screen in setup.roots() {
+        let window = root_screen.root();
+        let pointer_cookie = connection.send_request(&x::QueryPointer { window });
+        let geometry_cookie = connection.send_request(&x::GetGeometry {
+            drawable: x::Drawable::Window(window),
+        });
+
+        let pointer_reply = connection.wait_for_reply(pointer_cookie)?;
+        if pointer_reply.same_screen() {
+            let geometry_reply = connection.wait_for_reply(geometry_cookie)?;
+            let mut visualtype = match find_xcb_visualtype(&connection, root_screen.root_visual()) {
+                Some(visualtype) => visualtype,
+                None => continue,
+            };
+            let raw_connection = connection.get_raw_conn();
+            let width = geometry_reply.width() as i32;
+            let height = geometry_reply.height() as i32;
+
+            // SAFETY: cairo doesn't touch the pointers we give it, so
+            //      * the connection should be fine to pass through,
+            //      * the visualtype is (hopefully) ABI compatible with C
+            // Also see [1]
+            let screenshot = unsafe {
+                cairo_xcb_surface_create(
+                    raw_connection as _,
+                    window.resource_id(),
+                    &mut visualtype as *mut _ as _,
+                    width,
+                    height,
+                )
+            };
+
+            let surface_status = unsafe { cairo_surface_status(screenshot) };
+            if surface_status != CAIRO_STATUS_SUCCESS {
+                return Err(CairoError::from(surface_status).into());
+            }
+
+            let (file, stream) = gtk4::gio::File::new_tmp("screenshot.XXXXXX.png")?;
+            let path = file.path().unwrap();
+            let path = CString::new(path.as_os_str().as_bytes()).unwrap();
+
+            // SAFETY: * screenshot is a valid surface (see above the `cairo_surface_status` call)
+            //         *  path is a valid nul terminated c-string (we should've bailed out above otherwise)
+            match unsafe { cairo_surface_write_to_png(screenshot, path.as_ptr()) } {
+                0 => {}
+                err => return Err(CairoError::from(err).into()),
+            }
+
+            // Why do we this instead of just returning an XcbSurface?
+            // When I first started experimenting with writing a screenshot-utility, I did it in C++
+            // using xlib and Cairo::XlibSurface. That had some behaviour I disliked: when switching
+            // tags, the surface displayed the contents of the new tag, instead of the old one. This
+            // happened when I tested cairo-rs's XcbSurface, I assume it'll be the same, so we end
+            // up writing the screenshot to a .png and then reading it again
+
+            return Ok(ImageSurface::create_from_png(
+                &mut stream.input_stream().into_read(),
+            )?);
+        }
     }
 
-    // SAFETY: All the arguments are valid
-    let visual = unsafe { XDefaultVisual(display.as_ptr(), screen_where_the_cursor_is) };
+    Err(Error::FailedToTakeScreenshot)
+}
 
-    // SAFETY: All the arguments are valid, but I think cairo will return an error surface even if one is not?
-    let screenshot = unsafe {
-        ffi::cairo_xlib_surface_create(
-            display.as_ptr(),
-            window_where_cursor_is,
-            visual,
-            attributes.width,
-            attributes.height,
-        )
-    };
-    // Just to be sure
-    debug_assert!(!screenshot.is_null());
-
-    // SAFETY: `screenshot` is nonnull
-    match unsafe { ffi::cairo_surface_status(screenshot) } {
-        0 => {}
-        err => return Err(CairoError::from(err).into()),
+fn find_xcb_visualtype(conn: &xcb::Connection, visual_id: u32) -> Option<x::Visualtype> {
+    for root in conn.get_setup().roots() {
+        for depth in root.allowed_depths() {
+            for visual in depth.visuals() {
+                if visual.visual_id() == visual_id {
+                    return Some(*visual);
+                }
+            }
+        }
     }
 
-    let template = CString::new("screenshot.XXXXXX.png")
-        .expect("CString::new shouldn't fail with string literals AFAIK")
-        .into_raw();
-
-    // SAFETY: `path` is a nonnull, and a valid pointer. Also even if the template is invalid,
-    //         `mkstemps` should just return an error in that case.
-    let fd = unsafe { libc::mkstemps(template, 4) };
-    // SAFETY: We got this pointer earlier from calling CString::into_raw so this should be ok.
-    let path = unsafe { CString::from_raw(template) };
-    if fd == -1 {
-        return Err(io::Error::last_os_error().into());
-    }
-
-    // SAFETY: This is the only call to `from_raw_fd` for this specific fd, furthermore it's verified
-    //         to be a valid fd just above.
-    let mut file = unsafe { File::from_raw_fd(fd) };
-
-    // SAFETY: All the arguments are valid
-    match unsafe { cairo_surface_write_to_png(screenshot, path.as_ptr()) } {
-        0 => {}
-        err => return Err(CairoError::from(err).into()),
-    }
-
-    Ok(ImageSurface::create_from_png(&mut file)?)
+    None
 }
 
 /// Gets the screen resolution
@@ -151,62 +151,22 @@ pub fn take_screenshot() -> Result<ImageSurface, Error> {
 /// # Returns
 /// The first item of the tuple is the width, the second is the height
 pub fn get_screen_resolution() -> Result<(i32, i32), Error> {
-    let display = XDisplay::open_default()?;
-    let (window_where_cursor_is, _) = find_window_where_cursor_is(display.as_ptr())?;
-    // SAFETY: This is a C struct so this should be a valid byte pattern for it. I hope.
-    let mut attributes: XWindowAttributes = unsafe { std::mem::zeroed() };
-    // SAFETY: All the arguments are valid
-    let rc = unsafe {
-        XGetWindowAttributes(
-            display.as_ptr(),
-            window_where_cursor_is,
-            &mut attributes as _,
-        )
-    };
+    let (connection, _) = xcb::Connection::connect(None)?;
+    let setup = connection.get_setup();
 
-    if rc == 0 {
-        return Err(Error::FailedToGetWindowAttributes);
-    }
+    for root_screen in setup.roots() {
+        let window = root_screen.root();
+        let pointer_cookie = connection.send_request(&x::QueryPointer { window });
+        let geometry_cookie = connection.send_request(&x::GetGeometry {
+            drawable: x::Drawable::Window(window),
+        });
 
-    Ok((attributes.width, attributes.height))
-}
-
-fn find_window_where_cursor_is(display: *mut Display) -> Result<(Window, c_int), Error> {
-    // SAFETY: `display` isn't null, and it's obtained from a good call to XOpenDisplay, should be
-    //         good?
-    let screen_count = unsafe { XScreenCount(display) };
-
-    for screen in 0..screen_count {
-        let mut root_x = c_int::default();
-        let mut root_y = c_int::default();
-        let mut win_x = c_int::default();
-        let mut win_y = c_int::default();
-        let mut mask = c_uint::default();
-        let mut child = Window::default();
-        let mut window_with_cursor = Window::default();
-
-        // See:
-        //  * XQueryPointer: https://www.x.org/releases/X11R7.7/doc/man/man3/XQueryPointer.3.xhtml
-        // SAFETY: All arguments should be guaranteed valid, except for the value returned by XRootWindow
-        //         However, if XRootWindow returns an invalid Window, XQueryPointer will return an error
-        let rc = unsafe {
-            XQueryPointer(
-                display,
-                XRootWindow(display, screen),
-                &mut window_with_cursor as _,
-                &mut child as _,
-                &mut root_x as _,
-                &mut root_y as _,
-                &mut win_x as _,
-                &mut win_y as _,
-                &mut mask as _,
-            )
-        };
-
-        if rc == x11::xlib::True {
-            return Ok((window_with_cursor, screen));
+        let pointer_reply = connection.wait_for_reply(pointer_cookie)?;
+        if pointer_reply.same_screen() {
+            let geometry = connection.wait_for_reply(geometry_cookie)?;
+            return Ok((geometry.width() as i32, geometry.height() as i32));
         }
     }
 
-    Err(Error::FailedToGetRootWindow)
+    Err(Error::FailedToGetScreenResolution)
 }
