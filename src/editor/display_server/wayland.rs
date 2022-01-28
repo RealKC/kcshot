f62f074ba8c8a8777c00f60ac6886b85cb5b2dff
Mițca Dumitru <dumitru0mitca@gmail.com>
2022-01-28 22:18:28 +0200

    Open screenshots obtained from `org.freedesktop.Screenshot` properly
    This API returns a `file://` URI to the file created which
    `std::fs::File` cannot handle, so use gio APIs instead.
    
    Also delete the created file as this portal saves it in the user's home.

--- src/editor/display_server/wayland.rs
+++ src/editor/display_server/wayland.rs
@@ -1,5 +1,8 @@
 use cairo::ImageSurface;
-use gtk4::glib;
+use gtk4::{
+    gio, glib,
+    prelude::{FileExt, InputStreamExtManual},
+};
 
 use crate::kcshot::KCShot;
 
@@ -9,6 +12,8 @@ use super::{Result, Window, WmFeatures};
 pub enum Error {
     #[error("Encountered a desktop portal error: {0}")]
     Ashpd(#[from] ashpd::Error),
+    #[error("Failed opening file(uri={uri}) for reading: {error}")]
+    GioFile { error: glib::Error, uri: String },
 }
 
 pub(super) fn get_wm_features() -> Result<WmFeatures> {
@@ -27,8 +32,27 @@ pub(crate) fn take_screenshot(app: &KCShot) -> Result<ImageSurface> {
         .block_on(async { ashpd::desktop::screenshot::take(window_identifier, false, false).await })
         .map_err(Error::Ashpd)?;
 
-    let mut file = std::fs::File::open(&uri)?;
-    Ok(ImageSurface::create_from_png(&mut file)?)
+    let file = gio::File::for_uri(&uri);
+    let read = file
+        .read(gio::Cancellable::NONE)
+        .map_err(|error| Error::GioFile {
+            error,
+            uri: uri.clone(),
+        })?;
+
+    // This is intentionally not using `?` to ensure screenshot file is deleted even if the surface can't be
+    // created.
+    let screenshot = ImageSurface::create_from_png(&mut read.into_read());
+
+    // The org.freedesktop.Screenshot portal places the screenshots inside the user's home instead of
+    // making temp files, so this is to ensure that they get deleted and the user's home isn't polluted.
+    glib::MainContext::default().spawn_local(async move {
+        if let Err(why) = file.delete_future(glib::PRIORITY_LOW).await {
+            tracing::error!("Failed to delete file {uri} due to {why}");
+        }
+    });
+
+    Ok(screenshot?)
 }
 
 pub(crate) fn get_windows() -> Result<Vec<Window>> {
