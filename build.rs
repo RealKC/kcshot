bb53f158d4268df13edd98a178f76014f922e563
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-02-15 15:14:55 +0200

    Update to `glib-build-tools` 0.17
    I forgot to do this in the big `gtk-rs` ecosystem upgrade since it
    doesn't depend on any of the crates.

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() {
     println!("cargo:rerun-if-changed=migrations/");
 
     glib_build_tools::compile_resources(
-        "resources",
+        &["resources"],
         "resources/resources.gresource.xml",
         "compiled.gresource",
     );
