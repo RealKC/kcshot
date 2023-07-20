42aa7a888d75fe4ae87db13df56a1cf4bc18fce2
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-07-20 23:15:24 +0300

    Fix meson build
    Sadly this involves adding more hacks to the build script, but oh well.

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 fn main() {
     // See: https://docs.rs/diesel_migrations/2.0.0-rc.1/diesel_migrations/macro.embed_migrations.html#automatic-rebuilds
     println!("cargo:rerun-if-changed=migrations/");
@@ -11,4 +13,10 @@ fn main() {
         "resources/resources.gresource.xml",
         "compiled.gresource",
     );
+
+    // A hack to make the build work when building using meson and blueprint-compiler isn't in the path
+    if let Ok(blueprint_path) = env::var("BLUEPRINT_PATH") {
+        let path = env::var("PATH").unwrap();
+        println!("cargo:rustc-env=PATH={path}:{blueprint_path}");
+    }
 }
