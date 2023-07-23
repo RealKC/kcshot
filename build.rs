0bb9395b5a97288a420fa277195be01162751b10
Mițca Dumitru <dumitru0mitca@gmail.com>
2023-07-23 23:46:32 +0300

    Add a context menu in the main history view
    Currently it allows one to copy the path to the screenshot or the
    screenshot itself, but it can be expanded.
    
    Related: Better history widgets #17

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
-use std::env;
+use core::panic;
+use std::{env, process::Command};
 
 fn main() {
     // See: https://docs.rs/diesel_migrations/2.0.0-rc.1/diesel_migrations/macro.embed_migrations.html#automatic-rebuilds
@@ -8,15 +9,51 @@ fn main() {
         println!("cargo:rustc-cfg=kcshot_linting");
     }
 
-    glib_build_tools::compile_resources(
-        &["resources"],
-        "resources/resources.gresource.xml",
-        "compiled.gresource",
-    );
+    let mut blueprintc = "blueprint-compiler".to_owned();
 
     // A hack to make the build work when building using meson and blueprint-compiler isn't in the path
     if let Ok(blueprint_path) = env::var("BLUEPRINT_PATH") {
         let path = env::var("PATH").unwrap();
         println!("cargo:rustc-env=PATH={path}:{blueprint_path}");
+        blueprintc = format!("{blueprint_path}/{blueprintc}");
+    }
+
+    let blueprint_dir = env::var("OUT_DIR").unwrap() + "/resources";
+    compile_blueprints(
+        &blueprintc,
+        &["src/history/context_menu.blp"],
+        &blueprint_dir,
+    );
+
+    glib_build_tools::compile_resources(
+        &["resources", &blueprint_dir],
+        "resources/resources.gresource.xml",
+        "compiled.gresource",
+    );
+}
+
+fn compile_blueprints(blueprintc: &str, blueprints: &[&str], target: &str) {
+    let mut blueprintc = Command::new(blueprintc);
+    // blueprintc.args(["batch-compile", target, "src/"]);
+    blueprintc.arg("batch-compile");
+    blueprintc.arg(target);
+    blueprintc.arg("src/");
+
+    for blueprint in blueprints {
+        println!("cargo:rerun-if-changed={blueprint}");
+        blueprintc.arg(blueprint);
+    }
+
+    let output = blueprintc
+        .output()
+        .expect("Failed to execute blueprint-compiler");
+
+    if !output.status.success() {
+        panic!(
+            "blueprint-compiler returned {}, with stdout=:\n{}\n\t and stderr:\n{}",
+            output.status,
+            std::str::from_utf8(&output.stdout).unwrap(),
+            std::str::from_utf8(&output.stderr).unwrap()
+        );
     }
 }
---
+++ src/history/context_menu.rs
@@ -0,0 +1,56 @@
+use gtk4::{gdk, gio, prelude::*};
+
+use crate::historymodel::RowData;
+
+/// Creates a context menu with the usual operations you'd expect from a context menu on a history entry
+pub fn context_menu(data: RowData, parent: &gtk4::Widget) -> gtk4::PopoverMenu {
+    let builder = gtk4::Builder::from_resource("/kc/kcshot/ui/history/context_menu.ui");
+    let model = builder
+        .object::<gio::MenuModel>("history_context_menu")
+        .unwrap();
+
+    let menu = gtk4::PopoverMenu::builder()
+        .menu_model(&model)
+        .autohide(true)
+        .has_arrow(false)
+        .halign(gtk4::Align::Start)
+        .build();
+    menu.set_parent(parent);
+
+    let actions = gio::SimpleActionGroup::new();
+    menu.insert_action_group("ctx_menu", Some(&actions));
+
+    let copy_path = gio::SimpleAction::new("copy_path", None);
+    let path = data.path();
+    copy_path.connect_activate({
+        let menu = menu.clone();
+        move |_, _| {
+            menu.popdown();
+            if let Some(path) = &path {
+                clipboard().set_text(path);
+            }
+        }
+    });
+    actions.add_action(&copy_path);
+
+    let copy_image = gio::SimpleAction::new("copy_image", None);
+    let path = data.path();
+    copy_image.connect_activate({
+        let menu = menu.clone();
+        move |_, _| {
+            menu.popdown();
+            if let Some(path) = &path {
+                clipboard()
+                    .set_texture(&gdk::Texture::from_file(&gio::File::for_path(path)).unwrap());
+            }
+        }
+    });
+    actions.add_action(&copy_image);
+
+    menu
+}
+
+#[track_caller]
+fn clipboard() -> gdk::Clipboard {
+    gdk::Display::default().unwrap().clipboard()
+}
